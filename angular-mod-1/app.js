891f9ba7a77ca319380b7b6832c222066b33b8db
cu-dotcom <aishwaryasutar448@gmail.com>
2020-08-10 19:49:31 +0530

    1st module

---
+++ angular-mod-1/app.js
@@ -0,0 +1,21 @@
+(function(){
+'use strict';
+
+angular.module('LunchCheck',[])
+.controller('LunchCheckController',['$scope',function($scope){
+	$scope.data="";  //property is data
+	$scope.check= function(){
+		var items= $scope.data.split(',');
+		console.log(items);
+		if(items.length==0){
+			return "please enter data first";
+		}else if(items.length<=3 && items.length!=0 ) {
+			return "enjoy";
+		}else{
+			return "too much!";
+		}
+	};	
+
+}]);
+
+})();
