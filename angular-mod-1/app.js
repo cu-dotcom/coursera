7cdcc561279c3b38c97af4098f754021c1508bc3
cu-dotcom <aishwaryasutar448@gmail.com>
2020-08-10 20:22:50 +0530

    small changes

--- angular-mod-1/app.js
+++ angular-mod-1/app.js
@@ -4,15 +4,25 @@
 angular.module('LunchCheck',[])
 .controller('LunchCheckController',['$scope',function($scope){
 	$scope.data="";  //property is data
+	$scope.msg="";
+	$scope.msgstyle="";
+	$scope.boxstyle="";
 	$scope.check= function(){
-		var items= $scope.data.split('');
+		var items= $scope.data.split(',').filter(x=>x.trim()!="");
+
 		console.log(items);
 		if(items.length==0){
-			return "please enter data first";
-		}else if(items.length<=3 && items.length!=0 ) {
-			return "enjoy";
+			$scope.msgstyle={'color':'red'};
+			$scope.boxstyle={'border':'red'};
+			$scope.msg= "please enter data first";
+		}else if(items.length<=3) {
+			$scope.msgstyle={'color':'green'};
+			$scope.boxstyle={'border':'green'};
+			$scope.msg= "enjoy";
 		}else{
-			return "too much!";
+			$scope.msgstyle={'color':'green'};
+			$scope.boxstyle={'border':'green'};
+			$scope.msg= "too much!";
 		}
 	};	
 
